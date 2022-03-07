5e548cdae763804e7686e931711053208a681455
Weiting Lo <58645904+WeitingL@users.noreply.github.com>
2022-03-07 15:30:39 +0800

    Reset the test

--- app/src/main/java/com/weiting/cicdtest/MainActivity.kt
+++ app/src/main/java/com/weiting/cicdtest/MainActivity.kt
@@ -7,11 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val testEverything = "No bug!!!"
-        val alphabetTest = "D"
-        var numberTest = 4
 
-        val test = "No conflict"
 
     }
 }
